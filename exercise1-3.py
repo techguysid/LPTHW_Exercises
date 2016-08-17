894bbd0e20a2a400beccf7b870615c74f9bd29f0
Siddharth Udeniya <techguysid.23@gmail.com>
2016-08-18 00:51:48 +0530

    Added exericse 1-3

---
+++ exercise1-3.py
@@ -0,0 +1,24 @@
+#Exercise 1
+
+print "This is first exercise.Gives an introduction to priniting statements.I'm using python 2."
+
+#Exercise 2
+
+#This is a comment (Single Line Comment).
+
+#Exercise 3
+print "Exercise 2 was all about comments and hence not visible.This is Exercise 3."
+print "I will now count my chickens:"
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+print "Now I will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?", 5 > - 2
+print "Is it greater or equal?", 5 >= - 2
+print "Is it less or equal?", 5 <= - 2
